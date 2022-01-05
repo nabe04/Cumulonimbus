5a4bfc2d224cc6144f353c8e5875d7fc36f60c53
nabe04 <ryoki.tel.11528@gmail.com>
2022-01-06 02:20:33 +0900

    ビヘイビアツリークラス Ver1.0 完成

--- Source/Engine/behavior_tree.h
+++ Source/Engine/behavior_tree.h
@@ -34,24 +34,75 @@ namespace cumulonimbus::system
 
 		void Update(Args... args)
 		{
+			// ビヘイビアの処理が登録されていない場合処理を中断する
 			if (!behaviors.contains(current_behavior))
 				//return;
 				assert(!"Not found behavior");
 
-			behaviors.at(current_behavior).at(current_behavior_index)(args...);
+			// 現在のビヘイビアが全て終了しているので処理を飛ばす
+			if (is_completed)
+				return;
+
+			// ビヘイビアが変更された処理の初めのフレームの場合
+			if (is_switched)
+			{
+				// 初期フラグをtrueにする
+				is_start    = true;
+				is_switched = false;
+			}
+
+			// ビヘイビアツリー更新処理
+			behaviors.at(current_behavior).at(index)(args...);
+
+			// 初期フラグがTrueの場合Falseに
+			if (is_start)
+				is_start = false;
 		}
 
 		void NextSequence()
 		{
+			++index;
+			is_switched = true;
+
+			if (index <= behaviors.at(current_behavior).size())
+			{
+				index = 0;
+				is_completed = true;
+			}
+		}
+
+		void SetBehavior(const Key behavior)
+		{
+			if(!behaviors.contains(behavior))
+				assert(!"Not found behavior");
+
+			old_behavior		= current_behavior;
+			current_behavior	= behavior;
+			index				= 0;
+			is_switched			= true;
+			is_completed		= false;
 
 		}
 
+		[[nodiscard]]
+		bool GetIsCompleted() const
+		{
+			return is_completed;
+		}
 	private:
+		// 現在のビヘイビア状態
 		Key current_behavior{};
+		// 過去のビヘイビア状態
 		Key old_behavior{};
-
-		u_int current_behavior_index{ 0 };
-
+		// ビヘイビアツリーの要素番号
+		u_int index{ 0 };
+		// ビヘイビアが変更されたか
+		bool is_switched{ false };
+		// ビヘイビアが変更された最初のフレームか
+		bool is_start{ false };
+		// 現在のビヘイビアツリーに登録されている処理が全て完了したか
+		bool is_completed{ false };
+		// ビヘイビアツリー本体
 		using Sequence = std::vector<std::function<ReturnVal(Args...)>>;
 		std::map<Key, Sequence> behaviors{};
 	};
